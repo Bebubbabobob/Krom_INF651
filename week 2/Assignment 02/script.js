df742725565401dae808db6292b7fae172133536
Bebubbabobob <114362088+Bebubbabobob@users.noreply.github.com>
2026-09-04 17:35:42 -0500

    Add files via upload

---
+++ week 2/Assignment 02/script.js
@@ -0,0 +1,35 @@
+let num1 = 5;
+let num2 = 3;
+console.log("number1 is " + num1);
+console.log("number2 is " + num2);
+console.log("number1 + number 2 =  " + (num1 + num2));
+console.log("number1 - number 2 =  " + (num1 - num2));
+console.log("number1 * number 2 =  " + (num1 * num2));
+console.log("number1 / number 2 =  " + (num1 / num2));
+const string = "Peter Piper picked a peck of pickled peppers"
+console.log("The String is " + string)
+console.log("String Length: " + string.length)
+console.log("String's First Character: " + string.charAt(0))
+console.log("String's Last Character: " + string.charAt(43))
+let num3 = -9;
+console.log("number3 is " + num3);
+console.log("SQRT of number3 = " + Math.sqrt(num3))
+console.log("number3 squared = " + Math.pow(num3,2))
+console.log("absolute value of number3 = " + Math.abs(num3))
+let num4 = 32;
+let num5 = 56;
+console.log("number4 is " + num4);
+console.log("number5 is " + num5);
+console.log("number4 > number5 = " + (num4 > num5))
+console.log("number4 < number5 = " + (num4 < num5))
+console.log("number4 == number5 = " + (num4 == num5))
+const bool1 = true;
+const bool2 = false;
+console.log("bool1 = " + bool1)
+console.log("bool2 = " + bool2)
+console.log("bool1 and bool2 = true: " + (bool1 && bool2))
+console.log("bool1 or bool2 = true: " + (bool1 || bool2))
+console.log("bool1 is not true: " + !bool1)
+const firstname = "Caven"
+const lastname = "Krom"
+console.log("Hello my name is " + firstname + " " + lastname + ". It is nice to meet you.")
